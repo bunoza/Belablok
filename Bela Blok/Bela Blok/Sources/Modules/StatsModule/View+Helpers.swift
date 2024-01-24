f00e9d30b0f28117212c0d8697d625d16f2b9cf0
Domagoj Bunoza <domagoj.bunoza@rise-world.com>
2024-01-24 17:29:40 +0100

    Remove obsolete

--- Bela Blok/Bela Blok/Sources/Modules/StatsModule/View+Helpers.swift
+++ Bela Blok/Bela Blok/Sources/Modules/StatsModule/View+Helpers.swift
@@ -8,24 +8,4 @@ extension View {
         renderer.scale = 3.0
         return Image(uiImage: renderer.uiImage ?? UIImage())
     }
-
-    func hiddenNavigationLink<DestinationView: View>(
-        isLinkActive: Binding<Bool>,
-        destination: DestinationView
-    ) -> some View {
-        background(
-            NavigationLink(destination: destination, isActive: isLinkActive) {
-                EmptyView()
-            }
-            .isDetailLink(false)
-            .hidden()
-        )
-    }
-
-//    func setGreenBackground() -> some View {
-//        ZStack {
-//            Color.green.opacity(0.7).ignoresSafeArea(.all)
-//            self
-//        }
-//    }
 }
