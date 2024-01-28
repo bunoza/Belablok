5b5663e77467463c1670e4b79d110d1b3ffa2ec7
Domagoj Bunoza <domagoj.bunoza@rise-world.com>
2024-01-28 23:21:30 +0100

    Add stiglja and prepare for tooltip

--- Bela Blok/Bela Blok/Sources/Modules/MainModule/SupportingView/ResultRow.swift
+++ Bela Blok/Bela Blok/Sources/Modules/MainModule/SupportingView/ResultRow.swift
@@ -1,21 +1,27 @@
 import SwiftUI
 
 struct ResultRow: View {
+    @State private var showPadTooltip: Bool = false
+    @State private var showStigljaTooltip: Bool = false
+    
     private var numberOfGame: Int?
     private let weScore: String
     private let youScore: String
     private var showFallIcon: Bool?
+    private var showStigljaIcon: Bool?
 
     init(
         numberOfGame: Int? = nil,
         weScore: Int,
         youScore: Int,
-        showFallIcon: Bool = false
+        showFallIcon: Bool = false,
+        showStigljaIcon: Bool = false
     ) {
         self.numberOfGame = numberOfGame
         self.weScore = String(weScore)
         self.youScore = String(youScore)
         self.showFallIcon = showFallIcon
+        self.showStigljaIcon = showStigljaIcon
     }
 
     init(weLabel: String, youLabel: String) {
@@ -33,12 +39,24 @@ struct ResultRow: View {
 
                     Spacer()
 
-                    if let shouldShow = showFallIcon,
-                       shouldShow == true
-                    {
-                        Image(systemName: "figure.fall.circle")
-                            .font(.title2)
-                            .foregroundStyle(Color.red)
+                    Group {
+                        if let shouldShow = showStigljaIcon, shouldShow {
+                            Button {
+                                showStigljaTooltip = true
+                            } label: {
+                                Image(systemName: "theatermasks.fill")
+                                    .font(.title3)
+                                    .foregroundStyle(Color.red)
+                            }
+                        } else if let shouldShow = showFallIcon, shouldShow {
+                            Button {
+                                showPadTooltip = true
+                            } label: {
+                                Image(systemName: "figure.fall.circle")
+                                    .font(.title2)
+                                    .foregroundStyle(Color.red)
+                            }
+                        }
                     }
                 }
             }
